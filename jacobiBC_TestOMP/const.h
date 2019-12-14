6167a5585bd782a31abaa6a524e5815fed58ea7c
Paden <rumsey.pr@gmail.com>
2019-12-13 20:41:50 -0700

    added three different directories for the jacobi stuff. Manipulated jacobi code to get an iterative form of jacobi. Moving on to parallelizing the ringed version next.

---
+++ jacobiBC_TestOMP/const.h
@@ -0,0 +1,18 @@
+/****************************************************************************
+ * const.h                                                                  *
+ *                                                                          *
+ * Header file containing constants definition                              *
+ *                                                                          *
+ *    11/26/04 WQ -- file created                                           *
+ ****************************************************************************/
+
+#ifndef __CONST_H__
+#define __CONST_H__
+
+typedef unsigned int boolean;
+#define TRUE 1
+#define FALSE 0
+#define MATRIX_DIM 100
+
+#endif /* __CONST_H__ */
+
